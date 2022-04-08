d6fe113f595d66b5825307495f29fb2973dba14d
Daniel <danielsalazar284@gmail.com>
2022-04-07 20:43:30 -0400

    casi listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
-var tooltipTriggerList =
-[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-$(document).ready(function(){
+})
+$(document).ready(function () {
 
-    $("a").click(function (){
+    $("a").click(function () {
         var gato = this.hash
-       
+
         $("html, body").animate({
-            scrollTop: $(gato).offset().top - 10     },
-        800
-           )
- });
- $(window).scroll(function() {
-    if ($(document).scrollTop() > 900) { 
-    $(".navbar").css("background-color", "#000000");
-    } else {
-    $(".navbar").css("background-color", "transparent");
-    }
- });
-  
+                scrollTop: $(gato).offset().top - 10
+            },
+            800
+        )
+    });
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 900) {
+            $(".navbar").css("background-color", "#000000");
+        } else {
+            $(".navbar").css("background-color", "transparent");
+        }
+    });
+
 })
 
-$(document).ready(function(){
-    $(".btn").mouseup(function(){
-      alert("Mensaje enviado con exito");
+$(document).ready(function () {
+    $(".btn").mouseup(function () {
+        alert("Mensaje enviado con exito");
     });
-  });
+});
